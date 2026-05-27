23731701b249e162eca7ef720004fc4bb2ee21a1
icebell <iceiceicebell@gmail.com>
2026-05-27 13:40:58 +0800

    refactor(admin): routes

--- core/ui/context-menu.tsx
+++ core/ui/context-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ContextMenu as ContextMenuPrimitive } from "radix-ui";
+import * as ContextMenuPrimitive from "@radix-ui/react-context-menu";
 
 import { cn } from "@/core/lib/utils";
 
